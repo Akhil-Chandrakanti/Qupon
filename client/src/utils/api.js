7ef15f09716807e60c2e84c9976fb157678a88b0
Laxminath-Muvva <chandrakantiakhil@gmail.com>
2026-03-31 18:49:15 +0530

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -4,7 +4,12 @@ const api = axios.create({
   baseURL: "https://qupon-backend.onrender.com/api"
 });
 
-const token = localStorage.getItem('token');
-if (token) api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
 export default api;
